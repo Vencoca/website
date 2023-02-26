6e6cd0e4136cec76c337d5e8641108fdf98c87a9
vencoca <vaclav.kesler@gmail.com>
2023-02-26 16:17:39 +0100

    Desktop

--- js/scripts.js
+++ js/scripts.js
@@ -885,6 +885,31 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
 /*!
 
+ * CustomEase 3.11.4
+
+ * https://greensock.com
+
+ * 
+
+ * @license Copyright 2022, GreenSock. All rights reserved.
+
+ * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
+
+ * @author: Jack Doyle, jack@greensock.com
+
+ */
+
+
+
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self).window=e.window||{})}(this,function(e){"use strict";function m(e){return Math.round(1e5*e)/1e5||0}var b=/[achlmqstvz]|(-?\d*\.?\d*(?:e[\-+]?\d+)?)[0-9]/gi,w=/[\+\-]?\d*\.?\d+e[\+\-]?\d+/gi,Y=Math.PI/180,k=Math.sin,B=Math.cos,F=Math.abs,J=Math.sqrt;function arcToSegment(e,t,n,s,a,r,i,o,h){if(e!==o||t!==h){n=F(n),s=F(s);var u=a%360*Y,f=B(u),c=k(u),l=Math.PI,g=2*l,x=(e-o)/2,d=(t-h)/2,m=f*x+c*d,p=-c*x+f*d,y=m*m,M=p*p,v=y/(n*n)+M/(s*s);1<v&&(n=J(v)*n,s=J(v)*s);var C=n*n,E=s*s,b=(C*E-C*M-E*y)/(C*M+E*y);b<0&&(b=0);var w=(r===i?-1:1)*J(b),P=n*p/s*w,S=-s*m/n*w,N=f*P-c*S+(e+o)/2,D=c*P+f*S+(t+h)/2,T=(m-P)/n,V=(p-S)/s,_=(-m-P)/n,q=(-p-S)/s,A=T*T+V*V,R=(V<0?-1:1)*Math.acos(T/J(A)),G=(T*q-V*_<0?-1:1)*Math.acos((T*_+V*q)/J(A*(_*_+q*q)));isNaN(G)&&(G=l),!i&&0<G?G-=g:i&&G<0&&(G+=g),R%=g,G%=g;var L,O=Math.ceil(F(G)/(g/4)),j=[],z=G/O,I=4/3*k(z/2)/(1+B(z/2)),H=f*n,Q=c*n,Z=c*-s,U=f*s;for(L=0;L<O;L++)m=B(a=R+L*z),p=k(a),T=B(a+=z),V=k(a),j.push(m-I*p,p+I*m,T+I*V,V-I*T,T,V);for(L=0;L<j.length;L+=2)m=j[L],p=j[L+1],j[L]=m*H+p*Z+N,j[L+1]=m*Q+p*U+D;return j[L-2]=o,j[L-1]=h,j}}function stringToRawPath(e){function db(e,t,n,s){f=(n-e)/3,c=(s-t)/3,o.push(e+f,t+c,n-f,s-c,n,s)}var t,n,s,a,r,i,o,h,u,f,c,l,g,x,d,m=(e+"").replace(w,function(e){var t=+e;return t<1e-4&&-1e-4<t?0:t}).match(b)||[],p=[],y=0,M=0,v=m.length,C=0,E="ERROR: malformed path: "+e;if(!e||!isNaN(m[0])||isNaN(m[1]))return console.log(E),p;for(t=0;t<v;t++)if(g=r,isNaN(m[t])?i=(r=m[t].toUpperCase())!==m[t]:t--,s=+m[t+1],a=+m[t+2],i&&(s+=y,a+=M),t||(h=s,u=a),"M"===r)o&&(o.length<8?--p.length:C+=o.length),y=h=s,M=u=a,o=[s,a],p.push(o),t+=2,r="L";else if("C"===r)i||(y=M=0),(o=o||[0,0]).push(s,a,y+1*m[t+3],M+1*m[t+4],y+=1*m[t+5],M+=1*m[t+6]),t+=6;else if("S"===r)f=y,c=M,"C"!==g&&"S"!==g||(f+=y-o[o.length-4],c+=M-o[o.length-3]),i||(y=M=0),o.push(f,c,s,a,y+=1*m[t+3],M+=1*m[t+4]),t+=4;else if("Q"===r)f=y+2/3*(s-y),c=M+2/3*(a-M),i||(y=M=0),y+=1*m[t+3],M+=1*m[t+4],o.push(f,c,y+2/3*(s-y),M+2/3*(a-M),y,M),t+=4;else if("T"===r)f=y-o[o.length-4],c=M-o[o.length-3],o.push(y+f,M+c,s+2/3*(y+1.5*f-s),a+2/3*(M+1.5*c-a),y=s,M=a),t+=2;else if("H"===r)db(y,M,y=s,M),t+=1;else if("V"===r)db(y,M,y,M=s+(i?M-y:0)),t+=1;else if("L"===r||"Z"===r)"Z"===r&&(s=h,a=u,o.closed=!0),("L"===r||.5<F(y-s)||.5<F(M-a))&&(db(y,M,s,a),"L"===r&&(t+=2)),y=s,M=a;else if("A"===r){if(x=m[t+4],d=m[t+5],f=m[t+6],c=m[t+7],n=7,1<x.length&&(x.length<3?(c=f,f=d,n--):(c=d,f=x.substr(2),n-=2),d=x.charAt(1),x=x.charAt(0)),l=arcToSegment(y,M,+m[t+1],+m[t+2],+m[t+3],+x,+d,(i?y:0)+1*f,(i?M:0)+1*c),t+=n,l)for(n=0;n<l.length;n++)o.push(l[n]);y=o[o.length-2],M=o[o.length-1]}else console.log(E);return(t=o.length)<6?(p.pop(),t=0):o[0]===o[t-2]&&o[1]===o[t-1]&&(o.closed=!0),p.totalPoints=C+t,p}function p(){return M||"undefined"!=typeof window&&(M=window.gsap)&&M.registerPlugin&&M}function q(){(M=p())?(M.registerEase("_CE",n.create),a=1):console.warn("Please gsap.registerPlugin(CustomEase)")}function s(e){return~~(1e3*e+(e<0?-.5:.5))/1e3}function x(e,t,n,s,a,r,i,o,h,u,f){var c,l=(e+n)/2,g=(t+s)/2,d=(n+a)/2,m=(s+r)/2,p=(a+i)/2,y=(r+o)/2,M=(l+d)/2,v=(g+m)/2,C=(d+p)/2,E=(m+y)/2,b=(M+C)/2,w=(v+E)/2,P=i-e,S=o-t,N=Math.abs((n-i)*S-(s-o)*P),D=Math.abs((a-i)*S-(r-o)*P);return u||(u=[{x:e,y:t},{x:i,y:o}],f=1),u.splice(f||u.length-1,0,{x:b,y:w}),h*(P*P+S*S)<(N+D)*(N+D)&&(c=u.length,x(e,t,l,g,M,v,b,w,h,u,f),x(b,w,C,E,p,y,i,o,h,u,f+1+(u.length-c))),u}var M,a,t,y=/[-+=\.]*\d+[\.e\-\+]*\d*[e\-\+]*\d*/gi,v=/[cLlsSaAhHvVtTqQ]/g,n=((t=CustomEase.prototype).setData=function setData(e,t){t=t||{};var n,s,a,r,i,o,h,u,f,c=(e=e||"0,0,1,1").match(y),l=1,g=[],d=[],m=t.precision||1,p=m<=1;if(this.data=e,(v.test(e)||~e.indexOf("M")&&e.indexOf("C")<0)&&(c=stringToRawPath(e)[0]),4===(n=c.length))c.unshift(0,0),c.push(1,1),n=8;else if((n-2)%6)throw"Invalid CustomEase";for(0==+c[0]&&1==+c[n-2]||function _normalize(e,t,n){n||0===n||(n=Math.max(+e[e.length-1],+e[1]));var s,a=-1*e[0],r=-n,i=e.length,o=1/(+e[i-2]+a),h=-t||(Math.abs(e[i-1]-e[1])<.01*(e[i-2]-e[0])?function _findMinimum(e){var t,n=e.length,s=1e20;for(t=1;t<n;t+=6)+e[t]<s&&(s=+e[t]);return s}(e)+r:+e[i-1]+r);for(h=h?1/h:-o,s=0;s<i;s+=2)e[s]=(+e[s]+a)*o,e[s+1]=(+e[s+1]+r)*h}(c,t.height,t.originY),this.segment=c,r=2;r<n;r+=6)s={x:+c[r-2],y:+c[r-1]},a={x:+c[r+4],y:+c[r+5]},g.push(s,a),x(s.x,s.y,+c[r],+c[r+1],+c[r+2],+c[r+3],a.x,a.y,1/(2e5*m),g,g.length-1);for(n=g.length,r=0;r<n;r++)h=g[r],u=g[r-1]||h,(h.x>u.x||u.y!==h.y&&u.x===h.x||h===u)&&h.x<=1?(u.cx=h.x-u.x,u.cy=h.y-u.y,u.n=h,u.nx=h.x,p&&1<r&&2<Math.abs(u.cy/u.cx-g[r-2].cy/g[r-2].cx)&&(p=0),u.cx<l&&(u.cx?l=u.cx:(u.cx=.001,r===n-1&&(u.x-=.001,l=Math.min(l,.001),p=0)))):(g.splice(r--,1),n--);if(i=1/(n=1/l+1|0),h=g[o=0],p){for(r=0;r<n;r++)f=r*i,h.nx<f&&(h=g[++o]),s=h.y+(f-h.x)/h.cx*h.cy,d[r]={x:f,cx:i,y:s,cy:0,nx:9},r&&(d[r-1].cy=s-d[r-1].y);d[n-1].cy=g[g.length-1].y-s}else{for(r=0;r<n;r++)h.nx<r*i&&(h=g[++o]),d[r]=h;o<g.length-1&&(d[r-1]=g[g.length-2])}return this.ease=function(e){var t=d[e*n|0]||d[n-1];return t.nx<e&&(t=t.n),t.y+(e-t.x)/t.cx*t.cy},(this.ease.custom=this).id&&M&&M.registerEase(this.id,this.ease),this},t.getSVGData=function getSVGData(e){return CustomEase.getSVGData(this,e)},CustomEase.create=function create(e,t,n){return new CustomEase(e,t,n).ease},CustomEase.register=function register(e){M=e,q()},CustomEase.get=function get(e){return M.parseEase(e)},CustomEase.getSVGData=function getSVGData(e,t){var n,a,r,i,o,h,u,f,c,l,g=(t=t||{}).width||100,x=t.height||100,d=t.x||0,p=(t.y||0)+x,y=M.utils.toArray(t.path)[0];if(t.invert&&(x=-x,p=0),"string"==typeof e&&(e=M.parseEase(e)),e.custom&&(e=e.custom),e instanceof CustomEase)n=function rawPathToString(e){!function _isNumber(e){return"number"==typeof e}(e[0])||(e=[e]);var t,n,s,a,r="",i=e.length;for(n=0;n<i;n++){for(a=e[n],r+="M"+m(a[0])+","+m(a[1])+" C",t=a.length,s=2;s<t;s++)r+=m(a[s++])+","+m(a[s++])+" "+m(a[s++])+","+m(a[s++])+" "+m(a[s++])+","+m(a[s])+" ";a.closed&&(r+="z")}return r}(function transformRawPath(e,t,n,s,a,r,i){for(var o,h,u,f,c,l=e.length;-1<--l;)for(h=(o=e[l]).length,u=0;u<h;u+=2)f=o[u],c=o[u+1],o[u]=f*t+c*s+r,o[u+1]=f*n+c*a+i;return e._dirty=1,e}([e.segment],g,0,0,-x,d,p));else{for(n=[d,p],i=1/(u=Math.max(5,200*(t.precision||1))),f=5/(u+=2),c=s(d+i*g),a=((l=s(p+e(i)*-x))-p)/(c-d),r=2;r<u;r++)o=s(d+r*i*g),h=s(p+e(r*i)*-x),(Math.abs((h-l)/(o-c)-a)>f||r===u-1)&&(n.push(c,l),a=(h-l)/(o-c)),c=o,l=h;n="M"+n.join(",")}return y&&y.setAttribute("d",n),n},CustomEase);function CustomEase(e,t,n){a||q(),this.id=e,this.setData(t,n)}p()&&M.registerPlugin(n),n.version="3.11.4",e.CustomEase=n,e.default=n;if (typeof(window)==="undefined"||window!==e){Object.defineProperty(e,"__esModule",{value:!0})} else {delete e.default}});
+
+
+
+
+
+
+/*!
+
  * GSAP 3.11.4
 
  * https://greensock.com
@@ -956,9 +981,45 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
 
 
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother,CustomEase);
+
+
+
+window.scrollTo(0, 0);
+
+
+
+// create the scrollSmoother before your scrollTriggers
+
+let smoother = ScrollSmoother.create({
+
+    wrapper: "#smooth-wrapper",
+
+    content: "#smooth-content",
+
+    // normalizeScroll: true,   
+
+    // ignoreMobileResize: true,
+
+    smooth: 0.5,               // how long (in seconds) it takes to "catch up" to the native scroll position
+
+    effects: true,           // looks for data-speed and data-lag attributes on elements
+
+    // smoothTouch: 0.01,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+
+});
+
+
+
+
 const button = document.querySelector(".language_switcher");
 const circle = document.getElementById("lang_circle");
 const language_buttons = document.querySelectorAll(".lang_button")
+const language_toggles = document.querySelectorAll(".language_toggle")
 
 
 button.addEventListener( "click", () => {
@@ -966,13 +1027,15 @@ button.addEventListener( "click", () => {
     language_buttons.forEach(element => {
         element.classList.toggle("active");
     });
+    language_toggles.forEach(element => {
+        element.classList.toggle("en_language");
+    });
 } );
 
 
 const service_button = document.querySelector(".services_nav")
 service_button.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("click");
     gsap.to(smoother, {
         scrollTop: smoother.offset(".service", "bottom+=80 bottom"),
         duration: 1,
@@ -983,49 +1046,69 @@ service_button.addEventListener("click", (e) => {
 
 
 
-const nav_services = document.querySelector(".service_menu_fixed");
-const service_menu = document.querySelector(".service_menu");
-const wrapper = document.getElementById("smooth-wrapper");
-const second_nav_wrap = document.getElementById("second_nav_wrap");
-const nav_container = document.getElementById("nav_container");
 
-function swapelements(condition) {
-    if(condition){
-        service_menu.style.maxHeight=0;
-        nav_services.style.opacity = "100";
-        second_nav_wrap.style.paddingTop = "0px";
-        wrapper.style.marginTop = "200px";
-        hide_tween.invalidate();
-    } else {
-        service_menu.style.maxHeight="none";
-        nav_services.style.opacity = "0";
-        second_nav_wrap.style.paddingTop = "120px";
-        wrapper.style.marginTop = "80px";
-    }
-}
 
-const hide_tween = gsap.to(".smooth-wrapper",{
-    marginTop: 80,
-    onComplete: () => gsap.set(".service_menu_fixed",{opacity: 0}),
-    scrollTrigger: {
-        trigger: ".second_nav_wrap",
-        start: "bottom-=80 bottom-=200",
-        end: "bottom-=80 bottom-=360",
-        scrub: true,
-        onEnterBack: () => gsap.set(".service_menu_fixed",{opacity: 100})
-    }
+const loadTl = gsap.timeline({
+    delay: 0.8,
+    duration: 1.2,
+})
+
+
+loadTl.from(".smooth-wrapper",{
+    clipPath: "inset(0px 0px 0px 0px)",
+    duration: 1,
+}, "<");
+
+
+loadTl.from(".service__content" ,{
+    y: 200,
+    duration: 1.2,
+    ease: "power1.out",
+}, "<");
+
+
+loadTl.from(".hero__wrap__content",{
+    duration: 1,
+    yPercent: 100,
+    stagger: 0.1
+}, "<")
+  
+const service_toggles_footer = document.querySelectorAll(".footer-service-toggle")
+
+service_toggles_footer.forEach(element => {
+    element.addEventListener("click", (event)=>{
+        event.preventDefault()
+        service_boxes.forEach(element => {
+            element.classList.toggle("notactive");
+        });
+        service_toggles.forEach(element => {
+            element.classList.toggle("untoggled_service");
+        });
+        service_toggles_footer.forEach(element => {
+            element.classList.toggle("notactive");
+        })
+        gsap.to(smoother, {
+            scrollTop: smoother.offset(".bigtext", "top-=310 top"),
+            duration: 2,
+            ease: "inOut",
+        });
+    });
 });
-
 const contentWrapTl = gsap.timeline({
     duration: 1,
-    onComplete: () => swapelements(true),
+    onComplete: () => {
+        gsap.set("#smooth-wrapper", {clipPath: "inset(200px 0px 0px 0px)"})
+        gsap.set(".fixed", {maxHeight: 200})
+    },
     scrollTrigger: {
-        trigger: ".service_menu",
-        start: "bottom bottom-=80",
-        end: "top-=80 top",
+        trigger: ".service",
+        start: "bottom+=81 bottom",
+        end: "top+=98 top+=80",
         scrub: true,
-        invalidateOnRefresh: true, 
-        onEnterBack: () => swapelements(false),
+        onEnterBack: () => {
+            gsap.set("#smooth-wrapper", {clipPath: "inset(80px 0px 0px 0px)"})
+            gsap.set(".fixed", {maxHeight: 80})
+        },
     }
 })
 
@@ -1036,7 +1119,7 @@ contentWrapTl.to(".service_box",{
 },"<");
 
 contentWrapTl.to(".content-wrap-left",{
-    right: () => nav_container.offsetWidth/2 - 272*3/2,
+    right: () => document.getElementById("nav_container").offsetWidth/2 - 272*3/2,
     duration: 1,
 },"<");
 
@@ -1061,6 +1144,58 @@ contentWrapTl.to(".show_more",{
     marginBottom:0,
     duration: 0.3,
 },"<");
+
+contentWrapTl.to(".bigtext_container",{
+    y: -120,
+    ease: "power4.in",
+    duration: 1.2,
+},"<");
+document.getElementById('video').addEventListener("click", () => {    
+    let player = document.getElementById("ytplayer"); 
+    player.style.opacity = 100;
+    if (player.src.indexOf('autoplay=1') > -1){
+        player.src = player.src.replace('autoplay=1', '');;
+        player.style.opacity = 0;
+    } else {
+        let symbol = player.src.indexOf('?') > -1 ? '&' : '?';
+        player.src += symbol + 'autoplay=1';
+    }
+    gsap.to(smoother, {
+        scrollTop: smoother.offset(".video", "center-=200 center"),
+        duration: 1,
+        ease: "inOut",
+    });
+});
+
+gsap.to(".video_circle",{
+    width: 1560,
+    height: 1560,
+    scrollTrigger: {
+        trigger: "#video",
+        start: "top bottom",
+        end: "top top+=200",
+        scrub: true, 
+    }
+});
+const hideNavTl = gsap.timeline({
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".reasons",
+        start: "bottom+=260 center",
+        end: "bottom+=260 top+=80",
+        scrub: true,
+    }
+})
+
+hideNavTl.to(".fixed",{
+    duration: 1,
+    maxHeight: 80,
+},"<")
+
+hideNavTl.to("#smooth-wrapper",{
+    duration: 1,
+    clipPath: "inset(81px 0px 0px 0px)",
+},"<")
 const button1 = document.getElementById("where_first_button");
 const button2 = document.getElementById("where_second_button");
 
@@ -1075,32 +1210,247 @@ button2.addEventListener( "mouseout", () => {
     button2.classList.remove("active");
 } );
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const whereInTl = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".reasons",
+        start: "bottom bottom",
+        endTrigger: ".where",
+        end: "top+=350 top+=80",
+        scrub: true,
+    }
+})
+
+whereInTl.from(".where__circleWrap__innercircle",{
+    width: 0,
+    height: 0,
+    ease: CustomEase.create("later", "M0,0 C0.219,0.002 0.6,0 0.6,0 0.6,0 0.896,0.851 1,1"),
+    immediateRender: false,
+}, "<")
+
+whereInTl.from(".where__circleWrap__circle",{
+    width: 0,
+    height: 0,
+    ease: CustomEase.create("sooner", "M0,0,C0.068,0,0.2,0,0.2,0,0.2,0,0.5,0.6,0.5,0.6,0.5,0.6,0.999,0.998,1,1"),
+    immediateRender: false,
+}, "<")
+
+const wherePinTl = gsap.timeline({
+    duration: 10,
+    scrollTrigger: {
+        trigger: ".where",
+        pin: ".where ",
+        start: "top+=350 top+=80",
+        end: "+=650", 
+        scrub: true,
+       // markers: {startColor: "blue", endColor: "purple", fontSize: "18px", fontWeight: "bold", indent: 20} 
+    }
+})
+
+wherePinTl.to(".where__reveal__first",{
+    duration: 2,
+    opacity: 100,
+    y: 0,
+},"<")
+
+wherePinTl.to(".where__circleWrap__circle",{
+    duration: 10,
+    width: 1660,
+    height: 1660
+},"<")
+
+wherePinTl.to(".where__circleWrap__innercircle",{
+    duration: 10,
+    width: 1490,
+    height: 1490,
+},"<")
+
+wherePinTl.to(".where__reveal__first",{
+    duration: 4,
+    y: -100,
+},"<2")
+
+wherePinTl.to(".where__reveal__second",{
+    duration: 4,
+    y: -100,
+    opacity: 100,
+},"<")
 
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 
 
-// create the scrollSmoother before your scrollTriggers
+const whereOutTl = gsap.timeline({
+    duration: 10,
+    scrollTrigger:{
+        trigger: ".where",
+        start: "top+=351 top+=80",
+        endTrigger: ".partners",
+        end: "top top+=80",
+        scrub: true,
+        //markers: true,
+    }
+})
 
-let smoother = ScrollSmoother.create({
+whereOutTl.to(".where__circleWrap__circle",{
+    duration: 10,
+    width: 2420,
+    height: 3020,   
+    ease: CustomEase.create("custom", "M0,0,C0,0,0.155,0.095,0.198,0.138,0.406,0.346,1,1,1,1")
+},"<")
 
-    wrapper: "#smooth-wrapper",
+whereOutTl.to(".where__circleWrap__innercircle",{
+    duration: 10,
+    width: 2420,
+    height: 3020,
+    ease: CustomEase.create("custom", "M0,0,C0,0,0.155,0.095,0.198,0.138,0.406,0.346,1,1,1,1")
+},"<")
 
-    content: "#smooth-content",
+/*
+whereOutTl.to(".where__circleWrap__circle",{
+    duration: 4,
+    width: 2420,
+    height: 3020,   
+},"<")
 
-    // normalizeScroll: true,   
+whereOutTl.to(".where__circleWrap__innercircle",{
+    duration: 4,
+    width: 2420,
+    height: 3020,
+},"<")
 
-    // ignoreMobileResize: true,
+/*
+whereOutTl.to(".where__circleWrap__circle",{
+    duration: 4,
+    width: 1850,
+    height: 2050,   
+},"<")
 
-    smooth: 0.5,               // how long (in seconds) it takes to "catch up" to the native scroll position
+whereOutTl.to(".where__circleWrap__innercircle",{
+    duration: 4,
+    width: 1800,
+    height: 2000,
+},"<")
 
-    effects: true,           // looks for data-speed and data-lag attributes on elements
+whereOutTl.to(".where__circleWrap__innercircle",{
+    duration: 8,
+    y: 500,
+},"<")
 
-    // smoothTouch: 0.01,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+whereOutTl.to(".where__circleWrap__circle",{
+    duration: 8,
+    y: 500,
+},"<")
+
+whereOutTl.to(".where__circleWrap__circle",{
+    duration: 8,
+    width: 2420,
+    height: 3020,
+},"<4")
+
+whereOutTl.to(".where__circleWrap__innercircle",{
+    duration: 8,
+    width: 2420,
+    height: 3020,
+},"<")*/
+
+const service_boxes = document.querySelectorAll(".service_hover")
+
+const service_toggles = document.querySelectorAll(".service_toggle")
+
+const aditional_content = document.querySelectorAll(".aditional_content")
+
+
+
+service_boxes.forEach(element => {
+
+    element.addEventListener("mouseenter", hoverEnter);
+
+    element.addEventListener("mouseleave", hoverLeave);
+
+    element.addEventListener("click", swapOnClick);
 
 });
+
+function hoverEnter(event){
+
+    if (!ScrollTrigger.isScrolling()) {
+
+        if(event.target.classList.contains("notactive")){
+
+            event.target.classList.toggle("hovering")
+
+            service_boxes.forEach(element => {
+
+                element.classList.toggle("notactive");
+
+            });
+
+        }
+
+    }   
+
+}
+
+function hoverLeave(event){
+
+    if(event.target.classList.contains("hovering")){
+
+        event.target.classList.toggle("hovering")
+
+        service_boxes.forEach(element => {
+
+            element.classList.toggle("notactive");
+
+        });
+
+    }
+
+}
+
+function swapOnClick(event){
+
+    service_toggles_footer.forEach(element => {
+
+        element.classList.toggle("notactive");
+
+    })
+
+    aditional_content.forEach(element => {
+
+        element.classList.toggle("hidden")
+
+    })
+
+    if(event.target.closest('.service_hover').classList.contains("hovering")){
+
+        event.target.closest('.service_hover').classList.toggle("hovering")
+
+        service_toggles.forEach(element => {
+
+            element.classList.toggle("untoggled_service");
+
+        })
+
+    }
+
+    if(event.target.closest('.service_hover').classList.contains("service_box")){
+
+        gsap.to(smoother, {
+
+            scrollTop: smoother.offset(".bigtext", "top-=310 top"),
+
+            duration: 1,
+
+            ease: "inOut",
+
+        });
+
+    }
+
+    ScrollTrigger.refresh();
+
+}
 
 
 
