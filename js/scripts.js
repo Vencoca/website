e6344f8819527f920d692b3f8737826848ed8678
Václav Kesler <vaclav.kesler@gmail.com>
2023-05-09 10:22:43 +0200

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1001,7 +1001,7 @@ let smoother = ScrollSmoother.create({
 
     content: "#smooth-content",
 
-    normalizeScroll: true,   
+    //normalizeScroll: true,   
 
     //ignoreMobileResize: true,
 
@@ -1016,6 +1016,20 @@ let smoother = ScrollSmoother.create({
 
 
 smoother.paused(true);
+
+
+
+const appHeight = () => {
+
+    let doc = document.documentElement
+
+    // alert("new heigth: " + window.innerHeight)
+
+    doc.style.setProperty('--app-height', `${window.innerHeight*1}px`)
+
+}
+
+appHeight()
 
 
 const button = document.querySelector(".language_switcher");
@@ -1465,9 +1479,9 @@ mm3.add({
             duration: 10,
             scrollTrigger:{
                 trigger: ".where",
-                start: "top bottom",
+                start: "top+=100 bottom",
                 endTrigger: ".where",
-                end: "bottom top+=80",
+                end: "bottom+=100 top+=80",
                 scrub: true,
                 //markers: true,
             }  
